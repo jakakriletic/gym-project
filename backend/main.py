693108672213bf8773bcde6c35332be11d7c4978
jakak <jakak@LAPTOP-8K83PINM>
2025-05-27 11:15:07 +0200

    Baza + API + frontend

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from typing import Union
 from fastapi import FastAPI 
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+import mysql.connector
 
 app = FastAPI()
 
@@ -13,6 +14,14 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="jaka2003",
+  database="testbaza"
+)
+
 class Data(BaseModel):
     text1: str
     text2: str
@@ -20,4 +29,13 @@ class Data(BaseModel):
 @app.post("/process")
 async def read_root(data: Data):
     combined = f"{data.text1} {data.text2}"
+
+    mycursor = mydb.cursor()
+    sql = "INSERT INTO combinedString (id, cbString) VALUES(%s, %s)"
+    val = (3, combined)
+    mycursor.execute(sql, val)
+    mydb.commit()
+
     return {"Rezultat": combined}
+
+
